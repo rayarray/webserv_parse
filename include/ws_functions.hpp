0ab4acd82a054b8be12dda1fb6a4a22fa943362b
Riku Leskinen <rleskine@c1r6p3.hive.fi>
2024-05-28 11:35:41 +0300

    oops

---
+++ include/ws_functions.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+
+inline bool ws_wspace(char c) { return (c == ' ' || c == '\t'); }
+
+inline bool ws_checkword(const std::string &s, const std::vector<const std::string> &list) {
+	for (const std::string& word : list) {
+		if (s.find(word) == 0)
+			return true;
+	}
+	return false;
+}
+
